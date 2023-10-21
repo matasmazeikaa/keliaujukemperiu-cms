0ccb2a325043367db992d5f3318fef22b1d76f09
matasmazeikaa <matasmazeika@gmail.com>
2023-10-22 00:42:55 +0530

    uncomment urls

--- config/admin.js
+++ config/admin.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
-  // url: `${env("PUBLIC_URL")}/keliaujukemperiu/admin`,
+  url: `${env("PUBLIC_URL")}/keliaujukemperiu/admin`,
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
